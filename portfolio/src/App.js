e9fa7872d05e1d2bb05a9b026f04c88e5953e42b
weed7968 <tlstjdcjf980809@gmail.com>
2023-11-29 15:34:10 +0900

    가로 스크롤 구현

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,49 @@
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import styled from "styled-components";
 import Header from "./Components/Header";
 import MainPage from "./Page/MainPage";
 import AboutPage from "./Page/AboutPage";
 
 const App = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    let containerWidth = document.querySelector(".container").offsetWidth;
+    gsap.to(".container", {
+      scrollTrigger: {
+        trigger: ".container",
+        end: containerWidth,
+        pin: ".container",
+        scrub: 1,
+      },
+      x: -containerWidth,
+    });
+  }, []);
+
   return (
     <div>
       <Header />
-      <MainPage />
-      <AboutPage />
+      <Container className="container">
+        <Frame>
+          <MainPage />
+        </Frame>
+        <Frame>
+          <AboutPage />
+        </Frame>
+      </Container>
     </div>
   );
 };
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+const Frame = styled.div`
+  width: 100vw;
+`;
