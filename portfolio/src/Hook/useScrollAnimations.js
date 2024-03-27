ddbad5d3488526c75c9458200ad3f7abf4b44923
weed7968 <tlstjdcjf980809@gmail.com>
2024-03-27 18:35:25 +0900

    스크롤 애니메이션 훅 분리

---
+++ portfolio/src/Hook/useScrollAnimations.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+const useScrollAnimations = () => {
+  const [pageHeight, setPageHeight] = useState(0);
+  const [windowInnerHeight, setWindowInnerHeight] = useState(0);
+  const lenis = new Lenis();
+
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    let containerWidth = document.querySelector(".container").offsetWidth;
+    gsap.to(".container", {
+      scrollTrigger: {
+        trigger: ".container",
+        end: containerWidth,
+        pin: ".container",
+        scrub: 1,
+      },
+      x: -containerWidth,
+    });
+
+    const updateDimensions = () => {
+      setPageHeight(document.documentElement.scrollHeight);
+      setWindowInnerHeight(window.innerHeight);
+    };
+
+    updateDimensions();
+
+    window.addEventListener("resize", updateDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateDimensions);
+    };
+  }, []);
+
+  return { pageHeight, windowInnerHeight };
+};
+
+export default useScrollAnimations;
